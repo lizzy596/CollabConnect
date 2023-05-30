7e103daf18764b031f2b37e2180eab58ac2aaebc
lizzy596 <lizmeek123@gmail.com>
2023-05-30 17:19:47 -0500

    added config files

---
+++ src/config/tokens.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const tokenTypes = {
+  ACCESS: 'access',
+  REFRESH: 'refresh',
+  RESET_PASSWORD: 'resetPassword',
+  VERIFY_EMAIL: 'verifyEmail',
+};
